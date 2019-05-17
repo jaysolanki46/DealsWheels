2c0c29448f126b19cae7cbb1780eafad9f785395
jay solanki <jaysolanki46@gmail.com>
2019-05-17 12:01:11 +1200

    Animation added

--- app/src/main/java/com/example/jayso/dealswheels/CategoryAdapter.java
+++ app/src/main/java/com/example/jayso/dealswheels/CategoryAdapter.java
@@ -6,6 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +18,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
 
     private Context mContext;
     private List<Category> categories;
+    private int lastPosition = -5;
 
     public CategoryAdapter(Context mContext, List<Category> categories) {
         this.mContext = mContext;
@@ -37,6 +41,7 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
         myViewHolder.category_id.setText(categories.get(i).getCategory_id());
         myViewHolder.category_title.setText(categories.get(i).getCategory_title());
         myViewHolder.category_image.setImageResource(categories.get(i).getCategory_image());
+        setAnimation(myViewHolder.itemView, i);
     }
 
     @Override
@@ -56,6 +61,16 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
             category_id = (TextView) itemView.findViewById(R.id.category_id);
             category_image = (ImageView) itemView.findViewById(R.id.category_image);
             category_title = (TextView) itemView.findViewById(R.id.category_title);
+        }
+    }
+
+    private void setAnimation(View viewToAnimate, int position)
+    {
+        if (position > lastPosition)
+        {
+            Animation animation = AnimationUtils.loadAnimation(mContext, android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
         }
     }
 }
