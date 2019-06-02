7cb43949a6f25f193d2953fcb8dd6b1e5950fe75
jay solanki <jaysolanki46@gmail.com>
2019-06-02 23:23:05 +1200

    Voice search toasted

--- app/src/main/java/com/example/jayso/shopnsave/CategoryActivity.java
+++ app/src/main/java/com/example/jayso/shopnsave/CategoryActivity.java
@@ -2,18 +2,25 @@ package com.example.jayso.shopnsave;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class CategoryActivity extends AppCompatActivity {
 
@@ -72,5 +79,42 @@ public class CategoryActivity extends AppCompatActivity {
         MenuInflater menuInflater =  getMenuInflater();
         menuInflater.inflate(R.menu.activity_search, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.voice_bar_id:
+                getMic();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void getMic() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 10);
+        } else {
+            Toast.makeText(this, "Your device is not supported this feature !", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case 10:
+                if(resultCode == RESULT_OK && data != null){
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    // Grab voice result -> result.get(0)
+                    System.err.print(result.get(0));
+                }
+                break;
+        }
     }
 }
